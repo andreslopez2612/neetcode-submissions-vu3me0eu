cb5df9642b31f41921657e3e3ac100a1c7cbb27a
Brayan Andres Lopez Murcia <54787386+andreslopez2612@users.noreply.github.com>
2026-04-19 21:02:55 +1000

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.go
@@ -0,0 +1,35 @@
+func longestConsecutive(nums []int) int {
+
+	list := make(map[int][]int)
+
+	sort.Ints(nums)
+
+	n := 0
+
+	for i, v := range nums {
+		if len(list[n]) == 0 {
+			list[n] = append(list[n], v)
+			continue
+		}
+		if v == nums[i-1] {
+			continue
+		}
+		if v-1 == nums[i-1] {
+			list[n] = append(list[n], v)
+			continue
+		} else {
+			n++
+			list[n] = append(list[n], v)
+		}
+	}
+
+	result := 0
+
+	for i := range list {
+		if len(list[i]) > result {
+			result = len(list[i])
+		}
+	}
+
+	return result
+}
