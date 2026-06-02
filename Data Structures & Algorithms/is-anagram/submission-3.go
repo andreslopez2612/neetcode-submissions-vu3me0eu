0584de9b15fcb60746f851b51474b3410bf0d539
Brayan Andres Lopez Murcia <54787386+andreslopez2612@users.noreply.github.com>
2026-06-02 16:08:52 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.go
@@ -0,0 +1,24 @@
+import (
+	"slices"
+)
+
+
+func isAnagram(s string, t string) bool {
+
+	if len(s) == 0 || len(t) == 0 || len(s) != len(t) {
+		return false
+	}
+
+	v1, v2 := strings.Split(s, ""), strings.Split(t, "")
+
+	slices.Sort(v1)
+	slices.Sort(v2)
+
+	for v := range v1 {
+		if v1[v] != v2[v] {
+			return false
+		}
+	}
+
+	return true
+}
