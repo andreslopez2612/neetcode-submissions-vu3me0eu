595daffdca45eb7f04b35ce80a1954321edb9ae6
Brayan Andres Lopez Murcia <54787386+andreslopez2612@users.noreply.github.com>
2026-06-29 09:56:20 -0500

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.isEmpty() || t.isEmpty() || (s.length() != t.length())){
+            System.out.println("Pasa primer false");
+            return false;
+        }
+        
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        System.out.println(s1);
+        System.out.println(s2);
+
+        return Arrays.equals(s1, s2);
+    }
+}
